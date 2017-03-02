9075c1ad2c86c317062804f5ee7fbb0ab32a4ce2
Alberto Marin <hello@albertomar.in>
2017-03-02 17:40:33 +0000

    Add the create circles function

--- js/script.js
+++ js/script.js
@@ -1,40 +1,55 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function(){
 
-  var circleBackground = document.getElementsByClassName('circle-background')[0];
+  // var circleBackground = document.getElementsByClassName('circle-background')[0];
 
-//----------------------------------------------------
-//                      COLOR
-//----------------------------------------------------
+  //----------------------------------------------------
+  //                      CIRCLES
+  //----------------------------------------------------
 
-circleBackground.style['background-color'] = getRandomColor();
-console.log(circleBackground.style['background-color']);
+  // Add event listener to button
+  var button = document.getElementById("button-generator");
+  button.addEventListener("click", createCircles);
 
-function getRandomColor() {
-  var softColors = ["C","D","E","F"];
-  var randomColor = [];
-  for (var i = 0; i < 6; i++) {
-    randomColor[i] = softColors[Math.floor(Math.random() * softColors.length)];
-  }
-  return '#' + randomColor.join('');
+  function createCircles(){
+    var circleBackground = document.createElement('div');
+    circleBackground.className = "circle-background";
+    document.getElementById('random').appendChild(circleBackground);
 
-}
+    //----------------------------------------------------
+    //                      color
+    //----------------------------------------------------
 
-//----------------------------------------------------
-//                     POSITION
-//----------------------------------------------------
+    circleBackground.style['background-color'] = getRandomColor();
+    console.log(circleBackground.style['background-color']);
 
-  circleBackground.style.top = getRandomIntInclusive(0,50) + '%';
-  circleBackground.style.bottom = getRandomIntInclusive(0,50) + '%';
-  circleBackground.style.left = getRandomIntInclusive(0,50) + '%';
-  circleBackground.style.right = getRandomIntInclusive(0,50) + '%';
-  console.log(circleBackground.style.top + " : " + circleBackground.style.right + " : " + circleBackground.style.bottom + " : " + circleBackground.style.left);
+    function getRandomColor() {
+      var softColors = ["C","D","E","F"];
+      var randomColor = [];
+      for (var i = 0; i < 6; i++) {
+        randomColor[i] = softColors[Math.floor(Math.random() * softColors.length)];
+      }
+      return '#' + randomColor.join('');
 
-  // Getting a random integer between two values, inclusive
-  // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    //----------------------------------------------------
+    //                     position
+    //----------------------------------------------------
+
+    circleBackground.style.top = getRandomIntInclusive(0,50) + '%';
+    circleBackground.style.bottom = getRandomIntInclusive(0,50) + '%';
+    circleBackground.style.left = getRandomIntInclusive(0,50) + '%';
+    circleBackground.style.right = getRandomIntInclusive(0,50) + '%';
+    console.log(circleBackground.style.top + " : " + circleBackground.style.right + " : " + circleBackground.style.bottom + " : " + circleBackground.style.left);
+
+    // Getting a random integer between two values, inclusive
+    // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    function getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
   }
 
 });
